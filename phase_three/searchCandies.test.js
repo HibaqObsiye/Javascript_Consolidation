096ed1e1404165913aa76d4c4eaeeb7e8dc2c31e
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-17 16:49:47 +0000

    add edge case

--- phase_three/searchCandies.test.js
+++ phase_three/searchCandies.test.js
@@ -9,9 +9,13 @@ describe('searchCandies', () => {
         expect(searchCandies('Ma', 2)).toEqual(['Mars'])
     })
 
-    // it('returns skittles and starburst when the price is 10 and the prefix is S', () => {
-    //     expect()
-    // })
+    it('returns skittles and starburst when the price is 10 and the prefix is S', () => {
+        expect(searchCandies('S', 10)).toEqual(['Skitties', 'Skittles', 'Starburst' ])
+    })
+
+    it('returns skittles when the price is 4', () => {
+        expect(searchCandies('S', 4)).toEqual('Skittles', 'Skittles')
+    })
     
    
 
