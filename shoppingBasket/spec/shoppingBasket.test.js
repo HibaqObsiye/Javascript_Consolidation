852306e8938938bb5d5e48e6e55e69a2ba5231f5
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-17 19:49:06 +0000

    added test for multiple candy items

--- shoppingBasket/spec/shoppingBasket.test.js
+++ shoppingBasket/spec/shoppingBasket.test.js
@@ -8,12 +8,22 @@ describe('ShoppingBasket', () =>{
         expect(basket.getTotalPrice()).toEqual(0)
     })
 
-    it('adds candy items and returns total price', () => {
+    it('adds 1 candy item and returns total price', () => {
         const candy = new Candy('Mars', 4.99)
         const basket = new ShoppingBasket()
         basket.addItem(candy)
         
         expect(basket.getTotalPrice()).toEqual(4.99)
+    })
+    it('adds multiple candy items and returns a total price', ()=>{
+        const candy = new Candy('Mars', 4.99)
+        const candy2 = new Candy('Aero', 5)
+        const basket = new ShoppingBasket()
+        
+        basket.addItem(candy)
+        basket.addItem(candy2)
+
+        expect(basket.getTotalPrice()).toEqual(9.99)
     })
 
 })
