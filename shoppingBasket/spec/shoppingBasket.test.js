c5455b49a8e05d39e44523b187aacbf0c6122e1e
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-17 19:44:35 +0000

    implemented for loop to get failed test to pass

--- shoppingBasket/spec/shoppingBasket.test.js
+++ shoppingBasket/spec/shoppingBasket.test.js
@@ -3,9 +3,17 @@ const Candy = require('../lib/candy')
 
 describe('ShoppingBasket', () =>{
     it('returns the price of the candy', () =>{
-        const candy = new Candy('Mars', 4.99)
         const basket = new ShoppingBasket()
         
         expect(basket.getTotalPrice()).toEqual(0)
     })
+
+    it('adds candy items and returns total price', () => {
+        const candy = new Candy('Mars', 4.99)
+        const basket = new ShoppingBasket()
+        basket.addItem(candy)
+        
+        expect(basket.getTotalPrice()).toEqual(4.99)
+    })
+
 })
