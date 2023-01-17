d32d1de5273fa5e622a4e4efca7e069af29a4079
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-17 17:48:06 +0000

    add a getName test

--- shoppingBasket/spec/candy.test.js
+++ shoppingBasket/spec/candy.test.js
@@ -1,8 +1,13 @@
 const Candy = require('../lib/candy')
 
 describe('Class', () =>{
-    it("returns the name of the candy", () => {
+    it("returns the price of the candy", () => {
         candy = new Candy('Mars', 4.99)
         expect(candy.getPrice()).toEqual(4.99)
+    })
+
+    it("returns the name of the candy", () => {
+        candy = new Candy('Mars', 4.99)
+        expect(candy.getName()).toEqual('Mars')
     })
 })
