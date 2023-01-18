23b9147795dce43d4e18238031853cef248b6b78
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-18 11:35:57 +0000

    passing green test for thermostat get temperature function

--- shoppingBasket/spec/thermostat.test.js
+++ shoppingBasket/spec/thermostat.test.js
@@ -2,7 +2,7 @@ const Thermostat = require('../lib/thermostat')
 
 describe("Thermostat", () =>{
     it("starts with an intitial temperature of 20", () => {
-        thermostat = new Thermostat
+        const thermostat = new Thermostat() 
 
         expect(thermostat.getTemperature()).toEqual(20)
     })
