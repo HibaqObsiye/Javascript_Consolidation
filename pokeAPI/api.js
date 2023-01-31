0c54e94d16b11c4a83523257176f2ff8aa1fbac3
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-31 15:58:38 +0000

    create jest file for pokemon api

--- pokeAPI/api.js
+++ pokeAPI/api.js
@@ -1,7 +1,10 @@
+const { response } = require("express");
+
 const fetchPokemon = (name) =>{
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {let animal = {name: data.name, id: data.name, height: data.height, weight: data.weight}; return animal })
+    .then((pokemon) => console.log(pokemon))
 }
 
 module.exports = fetchPokemon
---
+++ pokeAPI/api.test.js
@@ -0,0 +1,1 @@
+const fetchPokemon = require('./api.js')
