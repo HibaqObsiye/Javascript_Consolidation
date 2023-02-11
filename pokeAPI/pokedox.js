3ceb9f8ef18c86320f0d95f62fc4a2319f2910eb
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-11 17:15:55 +0000

    adding catch and all function to pokedox api

--- pokeAPI/pokedox.js
+++ pokeAPI/pokedox.js
@@ -1,1 +1,16 @@
-const pokedox = 
+class Pokedox{
+    constructor(){
+        this.pokedox = []
+    }
+
+    catch(name){
+        
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((response) => response.json())
+        .then((data) => this.pokedox.push(data.name))
+    }
+    all(){
+        return this.pokedox
+    }
+}
+module.exports = Pokedox;
