ac89e81ed00c781b65199beb61949b6a5f1e9da0
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-11 16:58:49 +0000

    creating pokedox class

--- pokeAPI/api.js
+++ pokeAPI/api.js
@@ -3,7 +3,7 @@ const { response } = require("express");
 const fetchPokemon = (name) =>{
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
-    .then((pokemon)=> {const types = pokemon.types.map(type => type.type.name); console.log(pokemon.name, pokemon.id, pokemon.height, pokemon.weight, types)})
+    .then((pokemon)=> {const types = pokemon.types.map(type => type.type.name); console.log(pokemon.name, pokemon.id, pokemon.height, pokemon.weight, pokemon.types)})
     // .then((data) => console.log(data) )
 }
 
