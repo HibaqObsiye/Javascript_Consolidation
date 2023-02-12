6ab72cba8069a06f9c3e742224505870d09109f8
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-12 19:48:18 +0000

    added a get route

--- simple-server/server.js
+++ simple-server/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, express!')
+app.get('/', (req, res) => {    //app.get is a http request. It takes in the route and a callback function with two arguments, req and res. In order to send data back to the client, you have to use a method like res.send
+    return res.send("hello")
 });
 
 console.log(`Server listening on local host ${port} `)
