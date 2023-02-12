49c318bb9d77f9ff85b69c6733fdfac21e695b3f
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-12 18:32:21 +0000

    returning city temperatures

--- weatherApi/weatherClient.js
+++ weatherApi/weatherClient.js
@@ -11,12 +11,23 @@ class WeatherClient{
     }
 
     compareWith(city1, city2){
-        fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`)
-        .then((response)=> response.json())
-        .then((data)=> console.log(data.main.temp))
+        const temp = []
+         
+        const request1 = fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city1}&appid=${apiKey}`)
+                        .then((response)=> response.json())
+                        .then((data)=> temp.push(data.main.temp))
+        const request2 =    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city2}&appid=${apiKey}`)
+                          .then((response)=> response.json())
+                          .then((data)=> temp.push(data.main.temp))
+        
+        if(temp[0] > temp[1]){
+            return `${city1} is warmer with temp of ${temp[0]}`
+        }
+        else{
+            return `${city2} is warmer with temp of ${temp[1]}`
+        }
 
     }
-
 }
 
 module.exports = WeatherClient;
